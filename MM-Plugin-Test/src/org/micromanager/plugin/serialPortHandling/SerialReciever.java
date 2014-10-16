53acd743c21c9522d1077afcdad99c0b6745fc4b
Nopx <jollans94@googlemail.com>
2014-10-16 16:45:42 +0200

    commiting everything again without clear notes

---
+++ MM-Plugin-Test/src/org/micromanager/plugin/serialPortHandling/SerialReciever.java
@@ -0,0 +1,26 @@
+package src.org.micromanager.plugin.serialPortHandling;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class SerialReciever implements Observer {
+
+	   
+	   
+	public SerialReciever(){
+	    
+		
+	}
+	
+	public void printMsg(String msg){
+		
+	}
+
+
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
